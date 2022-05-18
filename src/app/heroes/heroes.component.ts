20bfa92f919914cb2d8b093ffaf06402979d7ad9
victor-h-huynh <victor.h.huynh@gmail.com>
2022-05-18 15:27:45 -0400

    navigation-completed

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,4 +1,3 @@
-import { MessageService } from './../message.service';
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero'
 import { HeroService } from '../hero.service';
@@ -9,18 +8,12 @@ import { HeroService } from '../hero.service';
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
-  selectedHero?: Hero;
   heroes: Hero[] = [];
 
-  constructor(private heroService: HeroService, private messageService: MessageService) { }
+  constructor(private heroService: HeroService) { }
 
   ngOnInit(): void {
     this.getHeroes();
-  }
-
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
-    this.messageService.add(`HeroesComponent: $elected hero id=${hero.id}`)
   }
 
   getHeroes(): void {

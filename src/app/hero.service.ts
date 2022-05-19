bec992906d656b3cd67e58854e254a038b5a8e26
victor-h-huynh <victor.h.huynh@gmail.com>
2022-05-19 15:25:32 -0400

    search-by-name

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -74,4 +74,22 @@ export class HeroService {
         catchError(this.handleError<any>('updateHero'))
       );
     }
+
+    /** POST: add a new hero to the server */
+    addHero(hero: Hero) : Observable<Hero> {
+      return this.http.post<Hero>(this.heroesUrl, hero, this.httpOptions).pipe(
+        tap((newHero: Hero) => this.log(`added hero w/ id=${newHero.id}`)),
+        catchError(this.handleError<Hero>('addHero'))
+      );
+    }
+
+    /** DELETE: delete the hero from the server */
+    deleteHero(id: number): Observable<Hero> {
+      const url = `${this.heroesUrl}/${id}`;
+
+      return this.http.delete<Hero>(url, this.httpOptions).pipe(
+        tap(_ => this.log(`deleted hero id=${id}`)),
+        catchError(this.handleError<Hero>('deleteHero'))
+      );
+    }
 }

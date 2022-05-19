0459d75c8aaaa57e891283bd56629626029b4cc1
victor-h-huynh <victor.h.huynh@gmail.com>
2022-05-19 14:35:20 -0400

    add-a-new-hero

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -17,10 +17,32 @@ export class HeroService {
 
   private heroesUrl = 'api/heroes';
 
+  httpOptions = {
+    headers: new HttpHeaders({ 'Content-Type': 'application/json'})
+  }
+
   constructor(
     private http: HttpClient,
     private messageService: MessageService
     ) { }
+
+    private log (message: string) {
+      this.messageService.add(`HeroService: ${message}`);
+    }
+
+    private handleError<T>(operation = 'operation', result?: T) {
+      return (error: any): Observable<T> => {
+
+        // TODO: send the error to remote logging infrastructure
+        console.error(error); // log to console instead
+
+        // TODO: better job of transforming error for user consumption
+        this.log(`${operation} failed: ${error.message}`);
+
+        // Let the app keep running by returning an empty result.
+        return of(result as T);
+      };
+    }
 
   getHeroes(): Observable<Hero[]> {
     return this.http.get<Hero[]>(this.heroesUrl)
@@ -44,21 +66,12 @@ export class HeroService {
     // return of (hero);
   }
 
-  private log (message: string) {
-    this.messageService.add(`HeroService: ${message}`);
-  }
 
-  private handleError<T>(operation = 'operation', result?: T) {
-    return (error: any): Observable<T> => {
-
-      // TODO: send the error to remote logging infrastructure
-      console.error(error); // log to console instead
-
-      // TODO: better job of transforming error for user consumption
-      this.log(`${operation} failed: ${error.message}`);
-
-      // Let the app keep running by returning an empty result.
-      return of(result as T);
-    };
-  }
+    /** PUT: update the hero on the server */
+    updateHero(hero: Hero): Observable<any> {
+      return this.http.put(this.heroesUrl, hero, this.httpOptions).pipe (
+        tap(_ => this.log(`updated hero id=${hero.id}`)),
+        catchError(this.handleError<any>('updateHero'))
+      );
+    }
 }
